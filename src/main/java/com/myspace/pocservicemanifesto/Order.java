f3bd67155a1ef81a56228754de9884608555d518
pam.admin <>
2019-01-02 21:18:09 +0000

    {/src/main/java/com/myspace/pocservicemanifesto/Order.java}

--- src/main/java/com/myspace/pocservicemanifesto/Order.java
+++ src/main/java/com/myspace/pocservicemanifesto/Order.java
@@ -6,12 +6,70 @@ package com.myspace.pocservicemanifesto;
 
 public class Order implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Order() {
-    }
+	@org.kie.api.definition.type.Label(value = "Order Number")
+	private long orderId;
+	@org.kie.api.definition.type.Label(value = "Requested Date")
+	private java.time.OffsetDateTime requestedDate;
+	@org.kie.api.definition.type.Label(value = "Order Date")
+	private java.time.OffsetDateTime orderDate;
+	@org.kie.api.definition.type.Label(value = "Shipping Condition")
+	private java.lang.String shippingCondition;
+	@org.kie.api.definition.type.Label(value = "Distance Between JobSite & Plant")
+	private long distanceJobSite_Plant;
 
+	public Order() {
+	}
 
+	public long getOrderId() {
+		return this.orderId;
+	}
 
+	public void setOrderId(long orderId) {
+		this.orderId = orderId;
+	}
+
+	public java.time.OffsetDateTime getRequestedDate() {
+		return this.requestedDate;
+	}
+
+	public void setRequestedDate(java.time.OffsetDateTime requestedDate) {
+		this.requestedDate = requestedDate;
+	}
+
+	public java.time.OffsetDateTime getOrderDate() {
+		return this.orderDate;
+	}
+
+	public void setOrderDate(java.time.OffsetDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public java.lang.String getShippingCondition() {
+		return this.shippingCondition;
+	}
+
+	public void setShippingCondition(java.lang.String shippingCondition) {
+		this.shippingCondition = shippingCondition;
+	}
+
+	public long getDistanceJobSite_Plant() {
+		return this.distanceJobSite_Plant;
+	}
+
+	public void setDistanceJobSite_Plant(long distanceJobSite_Plant) {
+		this.distanceJobSite_Plant = distanceJobSite_Plant;
+	}
+
+	public Order(long orderId, java.time.OffsetDateTime requestedDate,
+			java.time.OffsetDateTime orderDate,
+			java.lang.String shippingCondition, long distanceJobSite_Plant) {
+		this.orderId = orderId;
+		this.requestedDate = requestedDate;
+		this.orderDate = orderDate;
+		this.shippingCondition = shippingCondition;
+		this.distanceJobSite_Plant = distanceJobSite_Plant;
+	}
 
 }
